a9d04804e68b92d6c6f47d06b04c77b310a8d687
myico <huangyiqiang2018@163.com>
2020-06-10 15:35:42 +0800

    首次提交项目

---
+++ src/main/java/top/myico/sso/redissso/RedisSsoApplication.java
@@ -0,0 +1,13 @@
+package top.myico.sso.redissso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/myico/sso/redissso/RedisSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.myico.sso.redissso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
